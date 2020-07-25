05e9c225205cc393a8aadff69457a355ca11ddea
ritzdacanay24 <ritzdacanay24@yahoo.com>
2020-07-25 14:52:40 -0700

    Added bootstrap css files and added Header function component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Cards from './components/Cards';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Cards></Cards>
+
     </div>
   );
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,25 +1,46 @@
 import React from 'react';
 import axios from 'axios';
+import {
+    Card, CardImg, CardText, CardBody, CardLink,
+    CardTitle, CardSubtitle
+} from 'reactstrap';
 
 export default class Cards extends React.Component {
     state = {
         cards: []
     };
 
-    componentDidMount(){
+    componentDidMount() {
         axios.get('http://localhost:5000/api/collections')
             .then(res => {
                 const cards = res.data;
-                console.log(cards)
-                this.setState({cards})
+                this.setState({ cards })
             })
     }
     render() {
         return (
-          <ul>
-          { this.state.cards.map(person => <li>{person.title}</li>)}
-          </ul>
+            <div>
+                <div className="container">
+                    <div className="row">
+                        {this.state.cards.map(person =>
+                            <div className="col-lg-4">
+                                <Card>
+                                    <CardBody>
+                                        <CardTitle>{person.title}</CardTitle>
+                                        <CardSubtitle>Card subtitle</CardSubtitle>
+                                    </CardBody>
+                                    <img width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+                                    <CardBody>
+                                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                        <CardLink href="#">Card Link</CardLink>
+                                        <CardLink href="#">Another Link</CardLink>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
         )
-      }
-      
+    }
 }
