1f5d563fc192e929cd712474200579f3de14defa
ritzdacanay24 <ritzdacanay24@yahoo.com>
2020-07-27 22:52:40 -0700

    Add search field to search collections

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -35,17 +35,29 @@ export default class Cards extends React.Component {
         this.props.history.push('/CardsDetails', { cardId: cardId })
     }
 
+    search = () => {
+        console.log('asdf')
+    }
     render() {
         return (
             <div className="container">
+                <select onChange={(e) => this.goToCarddetails(e.target.value, 1)} name="collectionName" className="form-control center-block" style={{ width: "400px", margin: "0 auto" }}>
+                    <option defaultValue="" selected disabled>Select Collection</option>
+                    {
+                        this.state.cards.map((card, index) =>
+                            <option key={index} value={card._id}>{card.title}</option>
+                        )
+                    }
+                </select>
+            <br></br>
                 <div className="row">
                     {
                         this.state.cards.map((person, index) =>
                             <div className="col-lg-4 px-md-5" key={index}>
-                                <Card className="shadow p-3 mb-5 bg-white rounded" onClick={() => this.goToCarddetails(person._id, person.cards.length)}>
+                                <Card className="pointer shadow p-3 mb-5 bg-white rounded" onClick={() => this.goToCarddetails(person._id, person.cards.length)}>
                                     <CardBody>
-                                        <CardTitle><h3>{person.title}</h3></CardTitle>
-                                        <CardSubtitle>Total: {person.cards.length}</CardSubtitle>
+                                        <CardTitle style={{textAlign:"left"}}><h3>{person.title}</h3></CardTitle>
+                                        <CardSubtitle style={{position:"absolute", right:"20px", fontSize:"70px", top:"20px"}} className="inset">{person.cards.length}</CardSubtitle>
                                     </CardBody>
                                 </Card>
                             </div>
